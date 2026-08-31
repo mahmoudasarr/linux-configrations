016a1cb612d65c744ea2b35a65b5aee44b8ec60a
Mahmoud Asar <prowork.reconcile111@simplelogin.com>
2026-08-31 22:44:06 +0300

    fix: fix-bugs

--- new-fedora-setup.sh
+++ new-fedora-setup.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
+# ---- Remove Default Apps ----
 sudo dnf remove libreoffice* -y
 sudo dnf remove firefox -y
 sudo dnf upgrade --refresh -y
 
-#Nvidia
+# ---- Nvidia Drivers ----
 sudo dnf install \
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
@@ -12,44 +15,43 @@ sudo dnf install xorg-x11-drv-nvidia-cuda -y
 sudo akmods
 sudo dracut -f
 
-#Programming
-##VSCode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null -y
-dnf check-update &&
+# ---- Programming Tools ----
+## VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
 sudo dnf install code -y
 
-##Git
+## Git
 sudo dnf install git -y
 
-##C++
+## C++
 sudo dnf install gcc-c++ -y
 sudo dnf install @development-tools -y
 
-
-#Packages&Programs
+# ---- Packages & Programs ----
 sudo dnf group install multimedia -y
 sudo dnf install keepassxc -y
 sudo dnf install gthumb -y
 
-##Brave-Origin
+## Brave Browser
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
 sudo dnf install brave-origin -y
 
-##Mullvad-vpn
+## Mullvad VPN
 sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo -y
 sudo dnf install mullvad-vpn -y
 
-#Mullvad-browser
+## Mullvad Browser
 sudo dnf install mullvad-browser -y
 
-##VirtualBox
+## VirtualBox
 sudo dnf install kernel-devel kernel-headers dkms elfutils-libelf-devel qt5-qtx11extras -y
 sudo dnf install VirtualBox -y
 sudo usermod -aG vboxusers $USER
 
-##Flatpak-Apps
+## Flatpak Apps
 flatpak install flathub md.obsidian.Obsidian -y
 flatpak install flathub org.signal.Signal -y
 flatpak install flathub app.drey.Dialect -y
@@ -62,13 +64,9 @@ flatpak install flathub io.gitlab.librewolf-community -y
 flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak install flathub org.chromium.Chromium -y
 
-#Fonts
-
-##jetbrains-fonts
+# ---- Fonts ----
 sudo dnf install jetbrains-mono-fonts -y
+sudo dnf install google-noto-fonts-all -y
 
-##Google-fonts
-sudo dnf install -y google-noto-fonts-all -y
-
-#else
+# ---- Cleanup ----
 sudo dnf autoremove -y
